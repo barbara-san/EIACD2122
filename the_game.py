b6e783cc6c6ee14b4d7f2a035ff8594801c1170c
Barbara Santos <102589849+blsbls03@users.noreply.github.com>
2022-03-31 16:40:51 +0100

    Update the_game.py

--- the_game.py
+++ the_game.py
@@ -74,31 +74,31 @@ def mostra_tabul(atip, tab):
                  pygame.draw.ellipse(atip, (160, 213, 180), (j*sq+sq/2,i*sq+sq/2,sq/2-7,sq/2-7))
 
 		
-#le o jogo do ficheiro, não atualiza o jogador nem o numero da jogada
-def le_jogo(fich):
+#lê o jogo do ficheiro, não atualiza o jogador nem o numero da jogada
+def read_game(fich):
     f = open(fich, "r")
 
     if os.stat(fich).st_size == 0:
-        print("Impossivel ler o ficheiro %s") %fich   #cheka se o fich esta vazio ou não
+        print("Impossivel ler o ficheiro %s") %fich   #verifica se o ficheiro esta vazio ou não
 
     a = f.readline().split()  #not sure if this works, but i m separating everything 
-    N = int(a[0])                  # and expecting N to the the frist thing on the list xc
+    N = int(a[0])                  # and expecting N to be the first thing on the list xc
 
-    # a seria uma cena do genro = [N, 0000, 8880, 0000] , para um tabuleiro de 4x4
+    # a seria uma cena do genero = [N, 0000, 8880, 0000] , para um tabuleiro de 4x4
 
     a = a[1::] # i removed the 1st thing which again i assume its N so that the positions are alright now
 
     for i in range(N): 
-        for j in range(N):		# these for do not work, index out of range, sme thing for the next function, the rest is alright
-            tab[i][j] = a[i][j]		# thei re imcomplete tho
+        for j in range(N):		# these for do not work, index out of range, same thing for the next function, the rest is alright
+            tab[i][j] = a[i][j]		# they're imcomplete tho
 	
    
 #Grava o estado do jogo num ficheiro
-def garva_jogo():
+def grava_jogo():
     f = open(fich, "r")
 
     if os.stat(fich).st_size == 0:
-        print("Impossivel ler o ficheiro %s") %fich   #cheka se o fich esta vazio ou não
+        print("Impossivel ler o ficheiro %s") %fich   #verifica se o ficheiro esta vazio ou não
 
     a = f.readline().split() 
     N = int(a[0])            
